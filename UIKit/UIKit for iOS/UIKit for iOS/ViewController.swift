150c32fd58f1eb425fa546f22d4b51903b097c91
mrgsdev <mrgsdev@gmail.com>
2024-03-29 15:46:45 +0300

    Add Project

---
+++ UIKit/UIKit for iOS/UIKit for iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit for iOS
+//
+//  Created by mrgsdev on 29.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
