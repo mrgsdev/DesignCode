17eb8ae319fe7c7a5ea24d53a2aa8efe895cf5c8
mrgsdev <mrgsdev@gmail.com>
2024-04-02 13:15:20 +0300

    Introduction to Table Views

---
+++ UIKit/UIKit for iOS/UIKit for iOS/Cells/CoursesTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CoursesTableViewCell.swift
+//  UIKit for iOS
+//
+//  Created by mrgsdev on 02.04.2024.
+//
+
+import UIKit
+
+class CoursesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.shadowColor = UIColor(named: "Shadow")!.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: 0, height: 5)
+        layer.shadowRadius = 10
+        layer.masksToBounds = false
+        layer.cornerRadius = 30
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
