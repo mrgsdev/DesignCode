47b0c50ed4a230eae9608fe6d08021d34183f247
mrgsdev <mrgsdev@gmail.com>
2024-06-04 00:02:02 +0300

    Implementing Firebase Authentication

--- UIKit/UIKit for iOS/UIKit for iOS/View Controllers/LoginViewController.swift
+++ UIKit/UIKit for iOS/UIKit for iOS/View Controllers/LoginViewController.swift
@@ -6,34 +6,76 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Combine
 
 class LoginViewController: UIViewController {
-
+    
+    
     @IBOutlet weak var loginCard: CustomView!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var primaryButton: UIButton!
+    @IBOutlet weak var primaryBtn: UIButton!
     @IBOutlet weak var accessoryBtn: UIButton!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    var emailEmpty = true
+    var passwordEmpty = true
+    private var tokens: Set<AnyCancellable> = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         UIView.animate(withDuration: 1.0, delay: 0.5, options: .curveEaseInOut) {
             self.loginCard.alpha = 1.0
             self.loginCard.frame = self.loginCard.frame.offsetBy(dx: 0, dy: -400)
         }
+        emailField.publisher(for: \.text)
+            .sink { newValue in
+                if newValue != nil && newValue != "" {
+                    self.emailEmpty = false
+                } else {
+                    self.emailEmpty = true
+                }
+            }
+            .store(in: &tokens)
+        
+        passwordField.publisher(for: \.text)
+            .sink { newValue in
+                if newValue != nil && newValue != "" {
+                    self.passwordEmpty = false
+                } else {
+                    self.passwordEmpty = true
+                }
+            }
+            .store(in: &tokens)
+    }
+    @IBAction func primaryAction(_ sender: Any) {
+        if (emailEmpty || passwordEmpty) == true {
+            let alert = UIAlertController(title: "Missing Information", message: "Please make sure to enter a valid email and password", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            
+            Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { result, err in
+                if err != nil {
+                    print(err!.localizedDescription)
+                } else {
+                    self.goToHomeScreen()
+                }
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func accessoryAction(_ sender: Any) {
     }
-    */
-
+    
+    func goToHomeScreen() {
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CustomTabBarViewController") as! CustomTabBarViewController
+        vc.modalTransitionStyle = .flipHorizontal
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true)
+    }
 }
