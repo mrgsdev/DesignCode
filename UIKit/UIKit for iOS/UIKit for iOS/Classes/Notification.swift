6fca0672b93bd426aef0aeac46b2361b989bdfe9
mrgsdev <mrgsdev@gmail.com>
2024-06-06 18:24:08 +0300

    Introduction to Diffable Data Sources

---
+++ UIKit/UIKit for iOS/UIKit for iOS/Classes/Notification.swift
@@ -0,0 +1,30 @@
+//
+//  Notification.swift
+//  UIKit for iOS
+//
+//  Created by mrgsdev on 06.06.2024.
+//
+
+import UIKit
+import FirebaseFirestore
+ 
+
+enum TBSection: CaseIterable {
+    case main
+}
+
+struct NotificationModel: Codable, Hashable {
+    var id:String?
+    var title: String!
+    var subtitle: String!
+    var message: String!
+    var image: String!
+    var sentAt: Date!
+}
+
+let sampleNotifs = [
+    NotificationModel(id: "0x1", title: "New Release", subtitle: "SwiftUI", message: "A new release of the SwiftUI course has been released", image: "Logo SwiftUI", sentAt: Date()),
+    NotificationModel(id: "0x2", title: "Content Update", subtitle: "React Hooks", message: "The React Hooks course has been updated", image: "React", sentAt: Date()),
+    NotificationModel(id: "0x3", title: "New Release", subtitle: "UIKit for iOS 15", message: "A new release by Sai Kambampati has been pushed", image: "SwiftUI", sentAt: Date()),
+    NotificationModel(id: "0x4", title: "Black Friday Deal", subtitle: "Design+Code", message: "Purchase your subscription for 20% off only today", image: "Design+Code", sentAt: Date()),
+]
--- UIKit/UIKit for iOS/UIKit for iOS/View Controllers/NotificationsViewController.swift
+++ UIKit/UIKit for iOS/UIKit for iOS/View Controllers/NotificationsViewController.swift
@@ -6,24 +6,61 @@
 //
 
 import UIKit
+import Combine
 
 class NotificationsViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var cardView: CustomView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataSource: UITableViewDiffableDataSource<TBSection, NotificationModel>! = nil
+    var currentSnapshot: NSDiffableDataSourceSnapshot<TBSection, NotificationModel>! = nil
+    private var tokens: Set<AnyCancellable> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        
+        // Configure Data Source
+        self.dataSource = UITableViewDiffableDataSource<TBSection, NotificationModel>(tableView: tableView) {
+            (tableView: UITableView, indexPath: IndexPath, item: NotificationModel) -> NotificationTableViewCell in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationTableViewCell", for: indexPath) as? NotificationTableViewCell else {
+                fatalError("Can't create new cell")
+            }
+            
+            cell.titleLabel.text = item.title
+            cell.messageLabel.text = item.message
+            cell.subtitleLabel.text = item.subtitle.uppercased()
+            cell.banner.image = UIImage(named: item.image)
+            
+            return cell
+            
+        }
+        self.dataSource.defaultRowAnimation = .fade
+        
+        
+        // Subscribe to table view changes
+        tableView
+            .publisher(for: \.contentSize)
+            .sink { contentSize in
+                self.tableViewHeight.constant = contentSize.height + 10
+            }
+            .store(in: &tokens)
+        loadData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loadData()   {
+        currentSnapshot = NSDiffableDataSourceSnapshot<TBSection, NotificationModel>()
+        currentSnapshot.appendSections([.main])
+        self.currentSnapshot.appendItems(sampleNotifs, toSection: .main)
+        self.dataSource.apply(currentSnapshot, animatingDifferences: true)
     }
-    */
+    
+}
 
+extension NotificationsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
