494a8f4f89fb1f2030a59a303934325864db489b
mrgsdev <mrgsdev@gmail.com>
2024-06-07 09:36:17 +0300

    Setting up Firestore & Populating Data with Firestore

--- UIKit/UIKit for iOS/UIKit for iOS/View Controllers/NotificationsViewController.swift
+++ UIKit/UIKit for iOS/UIKit for iOS/View Controllers/NotificationsViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Combine
+import FirebaseFirestore
 
 class NotificationsViewController: UIViewController {
     
@@ -14,53 +15,74 @@ class NotificationsViewController: UIViewController {
     @IBOutlet weak var cardView: CustomView!
     @IBOutlet weak var tableView: UITableView!
     
-    var dataSource: UITableViewDiffableDataSource<TBSection, NotificationModel>! = nil
+    typealias diffableDS = UITableViewDiffableDataSource<TBSection, NotificationModel>
+    var dataSource: diffableDS! = nil
     var currentSnapshot: NSDiffableDataSourceSnapshot<TBSection, NotificationModel>! = nil
-    private var tokens: Set<AnyCancellable> = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.delegate = self
-        
-        // Configure Data Source
-        self.dataSource = UITableViewDiffableDataSource<TBSection, NotificationModel>(tableView: tableView) {
-            (tableView: UITableView, indexPath: IndexPath, item: NotificationModel) -> NotificationTableViewCell in
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationTableViewCell", for: indexPath) as? NotificationTableViewCell else {
-                fatalError("Can't create new cell")
-            }
-            
-            cell.titleLabel.text = item.title
-            cell.messageLabel.text = item.message
-            cell.subtitleLabel.text = item.subtitle.uppercased()
-            cell.banner.image = UIImage(named: item.image)
-            
-            return cell
-            
-        }
-        self.dataSource.defaultRowAnimation = .fade
-        
-        
-        // Subscribe to table view changes
-        tableView
-            .publisher(for: \.contentSize)
-            .sink { contentSize in
-                self.tableViewHeight.constant = contentSize.height + 10
-            }
-            .store(in: &tokens)
-        loadData()
-    }
-    
-    func loadData()   {
-        currentSnapshot = NSDiffableDataSourceSnapshot<TBSection, NotificationModel>()
-        currentSnapshot.appendSections([.main])
-        self.currentSnapshot.appendItems(sampleNotifs, toSection: .main)
-        self.dataSource.apply(currentSnapshot, animatingDifferences: true)
-    }
-    
-}
+       private var tokens: Set<AnyCancellable> = []
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           cardView.alpha = 0
+           tableView.delegate = self
+                   
+           // Configure Data Source
+           self.dataSource = diffableDS(tableView: tableView) {
+               (tableView: UITableView, indexPath: IndexPath, item: NotificationModel) -> NotificationTableViewCell in
+               guard let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationTableViewCell", for: indexPath) as? NotificationTableViewCell else {
+                   fatalError("Can't create new cell")
+               }
+               
+               cell.titleLabel.text = item.title
+               cell.messageLabel.text = item.message
+               cell.subtitleLabel.text = item.subtitle.uppercased()
+               cell.banner.image = UIImage(named: item.image)
+               
+               return cell
+               
+           }
+           self.dataSource.defaultRowAnimation = .fade
 
-extension NotificationsViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-}
+           // Load Firestore Data
+           Task {
+               do {
+                   try await self.loadData()
+                   self.cardView.alpha = 1
+               } catch let error {
+                   print(error.localizedDescription)
+               }
+           }
+           
+           // Subscribe to table view changes
+           tableView
+               .publisher(for: \.contentSize)
+               .sink { contentSize in
+               self.tableViewHeight.constant = contentSize.height + 10
+           }
+           .store(in: &tokens)
+       }
+       
+       func loadData() async throws {
+           currentSnapshot = NSDiffableDataSourceSnapshot<TBSection, NotificationModel>()
+           currentSnapshot.appendSections([.main])
+
+           let docs = try await Firestore.firestore()
+               .collection("notifications")
+               .order(by: "sentAt", descending: false)
+               .getDocuments()
+           var notifications = [NotificationModel]()
+
+           for docSnapshot in docs.documents {
+               let data = try docSnapshot.data(as: NotificationModel.self)
+               notifications.append(data)
+           }
+
+           self.currentSnapshot.appendItems(notifications, toSection: .main)
+           await self.dataSource.apply(currentSnapshot, animatingDifferences: true)
+       }
+   }
+
+   extension NotificationsViewController: UITableViewDelegate {
+       func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+           tableView.deselectRow(at: indexPath, animated: true)
+       }
+   }
