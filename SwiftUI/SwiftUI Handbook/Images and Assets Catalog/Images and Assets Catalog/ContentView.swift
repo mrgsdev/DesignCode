2e480b4ebdfd08df8ae372ba9b64835e6129e068
mrgsdev <mrgsdev@gmail.com>
2024-06-08 10:43:42 +0300

    Images and Assets Catalog

---
+++ SwiftUI/SwiftUI Handbook/Images and Assets Catalog/Images and Assets Catalog/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Images and Assets Catalog
+//
+//  Created by mrgsdev on 08.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("Illustration")
+        }
+        Spacer()
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/SwiftUI Handbook/Images and Assets Catalog/Images and Assets Catalog/Images_and_Assets_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Images_and_Assets_CatalogApp.swift
+//  Images and Assets Catalog
+//
+//  Created by mrgsdev on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Images_and_Assets_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
