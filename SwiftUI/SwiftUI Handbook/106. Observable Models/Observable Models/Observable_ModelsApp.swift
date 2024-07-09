3f282433947caf02efed4eb4d5a4e2b7fc99cbc5
mrgsdev <mrgsdev@gmail.com>
2024-07-09 14:07:06 +0300

    Observable Models

---
+++ SwiftUI/SwiftUI Handbook/106. Observable Models/Observable Models/Observable_ModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Observable_ModelsApp.swift
+//  Observable Models
+//
+//  Created by mrgsdev on 06.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Observable_ModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
