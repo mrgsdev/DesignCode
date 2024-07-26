40ea0d5a17bd590a2fdad6269b5cc43e3a86415f
mrgsdev <mrgsdev@gmail.com>
2024-07-26 18:54:00 +0300

    20

---
+++ SwiftUI/002. SwiftUI Advanced Handbook/20. Compress a UIImage/Compress a UIImage/Compress a UIImage/Compress_a_UIImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compress_a_UIImageApp.swift
+//  Compress a UIImage
+//
+//  Created by mrgsdev on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Compress_a_UIImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/002. SwiftUI Advanced Handbook/20. Compress a UIImage/Compress a UIImage/Compress a UIImage/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  Compress a UIImage
+//
+//  Created by mrgsdev on 19.07.2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension UIImage {
+    func aspectFittedToHeight(_ newHeight: CGFloat) -> UIImage {
+        let scale = newHeight / self.size.height
+        let newWidth = self.size.width * scale
+        let newSize = CGSize(width: newWidth, height: newHeight)
+        let renderer = UIGraphicsImageRenderer(size: newSize)
+
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: newSize))
+        }
+    }
+}
