0fde3bc51ec74b483d6023ecdc6bd765c80df825
mrgsdev <mrgsdev@gmail.com>
2024-07-28 11:59:40 +0300

    Add Project

---
+++ SwiftUI/003. Build an app with SwiftUI/Part 1/Build an app with SwiftUI/Build_an_app_with_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Build_an_app_with_SwiftUIApp.swift
+//  Build an app with SwiftUI
+//
+//  Created by mrgsdev on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Build_an_app_with_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/003. Build an app with SwiftUI/Part 1/Build an app with SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Build an app with SwiftUI
+//
+//  Created by mrgsdev on 28.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
