c9983a27fe9a4fcb1b0d9389ed10e0e52e53920e
mrgsdev <mrgsdev@gmail.com>
2024-07-29 12:03:35 +0300

    Color Literal, Gradient and Overlay

--- SwiftUI/003. Build an app with SwiftUI/Part 1/Build an app with SwiftUI/MenuView.swift
+++ SwiftUI/003. Build an app with SwiftUI/Part 1/Build an app with SwiftUI/MenuView.swift
@@ -13,22 +13,63 @@ struct MenuView: View {
             Spacer()
             
             VStack(spacing: 16) {
+                Text("Meng - 28% complete")
+                    .font(.caption)
+                
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(
+                        Color(
+                            #colorLiteral(
+                                red: 0,
+                                green: 0,
+                                blue: 0,
+                                alpha: 1
+                            )
+                        ).opacity(0.08)
+                    )
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(12)
+                
                 MenuRow(title: "Account", icon: "gear")
                 MenuRow(title: "Billing", icon: "creditcard")
                 MenuRow(title: "Sign out", icon: "person.crop.circle")
             }
             .frame(maxWidth: .infinity)
             .frame(height: 300)
-            .background(Color.white)
+            .background(
+                LinearGradient(
+                    gradient: Gradient(
+                        colors: [
+                            .white,
+                            .gray.opacity(0.1)
+                        ]
+                    ),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(radius: 30)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .padding(.horizontal, 30)
+            .overlay(
+                Image("Avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            )
         }
         .padding(.bottom, 30)
     }
 }
 
- 
 
 struct MenuRow: View {
     var title: String
@@ -40,6 +81,16 @@ struct MenuRow: View {
                 .font(.system(size: 20, weight: .light))
                 .imageScale(.large)
                 .frame(width: 32, height: 32)
+                .foregroundColor(
+                    Color(
+                        #colorLiteral(
+                            red: 0.662745098,
+                            green: 0.7333333333,
+                            blue: 0.831372549,
+                            alpha: 1
+                        )
+                    )
+                )
             
             Text(title)
                 .font(.system(size: 20, weight: .bold, design: .default))
