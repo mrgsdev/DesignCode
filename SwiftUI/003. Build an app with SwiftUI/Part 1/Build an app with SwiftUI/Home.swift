3d735b76fc3537e1b87516489c181ea6c17a7dc3
mrgsdev <mrgsdev@gmail.com>
2024-07-29 12:19:25 +0300

    Animation Between Screens

---
+++ SwiftUI/003. Build an app with SwiftUI/Part 1/Build an app with SwiftUI/Home.swift
@@ -0,0 +1,59 @@
+//
+//  Home.swift
+//  Build an app with SwiftUI
+//
+//  Created by mrgsdev on 29.07.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State var showProfile = false
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Text("Watching")
+                        .font(.system(size: 28, weight: .bold))
+                    
+                    Spacer()
+                    
+                    Button(action: { self.showProfile.toggle() }) {
+                        Image("Avatar")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 36, height: 36)
+                            .clipShape(Circle())
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 30)
+                
+                Spacer()
+            }
+            .padding(.top, 44)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .offset(y: showProfile ? -450 : 0)
+            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10.0, y: 0, z: 0))
+            .scaleEffect(showProfile ? 0.9 : 1)
+            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0),value: showProfile)
+            .edgesIgnoringSafeArea(.all)
+            
+            MenuView()
+                .background(Color.black.opacity(0.001))
+                .offset(y: showProfile ? 0 : 1000)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0),value: showProfile)
+                 
+        }
+    }
+}
+
+#Preview {
+    Home()
+}
