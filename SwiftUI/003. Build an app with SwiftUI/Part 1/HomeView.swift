9b006065443d22c7af9d8fbeeff6c7f5eca32fb3
mrgsdev <mrgsdev@gmail.com>
2024-07-30 10:04:53 +0300

    ScrollView and Repeat

--- SwiftUI/003. Build an app with SwiftUI/Part 1/HomeView.swift
+++ SwiftUI/003. Build an app with SwiftUI/Part 1/HomeView.swift
@@ -20,10 +20,49 @@ struct HomeView: View {
                 AvatarView(showProfile: $showProfile)
             }
             .padding(.horizontal)
+            .padding(.leading, 14)
             .padding(.top, 30)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(0 ..< 5) { item in
+                        SectionView()
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
             
             Spacer()
         }
+    }
+}
+struct SectionView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text("Prototype designs in SwiftUI")
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                Spacer()
+                Image("Logo1")
+            }
+            
+            Text("18 Sections".uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
     }
 }
 
