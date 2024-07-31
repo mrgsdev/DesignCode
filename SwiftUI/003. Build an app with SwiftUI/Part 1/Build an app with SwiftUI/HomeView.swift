f55642746d87f47f027cbae7a2a6fd3054a26972
mrgsdev <mrgsdev@gmail.com>
2024-07-31 22:07:37 +0300

    3D Scroll Animation

--- SwiftUI/003. Build an app with SwiftUI/Part 1/Build an app with SwiftUI/HomeView.swift
+++ SwiftUI/003. Build an app with SwiftUI/Part 1/Build an app with SwiftUI/HomeView.swift
@@ -26,7 +26,13 @@ struct HomeView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 30) {
                     ForEach(sectionData) { item in
-                        SectionView(section: item)
+                        GeometryReader { geometry in
+                            SectionView(section: item)
+                                .rotation3DEffect(Angle(degrees:
+                                                            Double(geometry.frame(in: .global).minX - 30) / -20
+                                                       ), axis: (x: 0, y: 10, z: 0))
+                        }
+                        .frame(width: 275, height: 275)
                     }
                 }
                 .padding(30)
