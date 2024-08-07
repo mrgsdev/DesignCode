2bda9f655cf276ff0c9a559e6e914a62a139be2d
mrgsdev <mrgsdev@gmail.com>
2024-08-07 11:29:23 +0300

    Dismiss Gesture with Scale

--- SwiftUI/003. Build an app with SwiftUI/Part 2/Build an app with SwiftUI/CourseList.swift
+++ SwiftUI/003. Build an app with SwiftUI/Part 2/Build an app with SwiftUI/CourseList.swift
@@ -11,10 +11,11 @@ struct CourseList: View {
     @State var courses = courseData
     @State var active = false
     @State var activeIndex = -1
+    @State var activeView = CGSize.zero
     
     var body: some View {
         ZStack {
-            Color.black.opacity(active ? 0.5 : 0)
+            Color.black.opacity(Double(self.activeView.height/500))
                 .animation(.linear)
                 .edgesIgnoringSafeArea(.all)
             
@@ -34,7 +35,8 @@ struct CourseList: View {
                                 course: self.courses[index],
                                 active: self.$active,
                                 index: index,
-                                activeIndex: self.$activeIndex
+                                activeIndex: self.$activeIndex,
+                                activeView: self.$activeView
                             )
                                 .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
                                 .opacity(self.activeIndex != index && self.active ? 0 : 1)
@@ -59,13 +61,13 @@ struct CourseList: View {
     CourseList()
 }
 
-
 struct CourseView: View {
     @Binding var show: Bool
     var course: Course
     @Binding var active: Bool
     var index: Int
     @Binding var activeIndex: Int
+    @Binding var activeView: CGSize
     
     var body: some View {
         ZStack(alignment: .top) {
@@ -126,7 +128,24 @@ struct CourseView: View {
                 .background(Color(course.color))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .shadow(color: Color(course.color).opacity(0.3), radius: 20, x: 0, y: 20)
-            
+            .gesture(
+                show ?
+                DragGesture().onChanged { value in
+                    guard value.translation.height < 300 else { return }
+                    guard value.translation.height > 0 else { return }
+                    
+                    self.activeView = value.translation
+                }
+                .onEnded { value in
+                    if self.activeView.height > 50 {
+                        self.show = false
+                        self.active = false
+                        self.activeIndex = -1
+                    }
+                    self.activeView = .zero
+                }
+                : nil
+            )
             .onTapGesture {
                 self.show.toggle()
                 self.active.toggle()
@@ -138,10 +157,32 @@ struct CourseView: View {
             }
         }
         .frame(height: show ? screen.height : 280)
+        .scaleEffect(1 - self.activeView.height / 1000)
+        .rotation3DEffect(Angle(degrees: Double(self.activeView.height / 10)), axis: (x: 0, y: 10.0, z: 0))
+        .hueRotation(Angle(degrees: Double(self.activeView.height)))
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        .gesture(
+            show ?
+            DragGesture().onChanged { value in
+                guard value.translation.height < 300 else { return }
+                guard value.translation.height > 0 else { return }
+                
+                self.activeView = value.translation
+            }
+            .onEnded { value in
+                if self.activeView.height > 50 {
+                    self.show = false
+                    self.active = false
+                    self.activeIndex = -1
+                }
+                self.activeView = .zero
+            }
+            : nil
+        )
         .edgesIgnoringSafeArea(.all)
     }
 }
+
 
 struct Course: Identifiable {
     var id = UUID()
