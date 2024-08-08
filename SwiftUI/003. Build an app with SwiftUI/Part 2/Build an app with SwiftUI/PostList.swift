8e9682d477365a6d8b04d30aad52f452259d7656
mrgsdev <mrgsdev@gmail.com>
2024-08-08 10:21:46 +0300

    API and Combine

--- SwiftUI/003. Build an app with SwiftUI/Part 2/Build an app with SwiftUI/PostList.swift
+++ SwiftUI/003. Build an app with SwiftUI/Part 2/Build an app with SwiftUI/PostList.swift
@@ -6,21 +6,32 @@
 //
 
 import SwiftUI
-
 struct PostList: View {
-    @State var posts: [Post] = []
+    @ObservedObject var store = DataStore()
     
     var body: some View {
-        List(posts) { post in
-            Text(post.title)
-        }
-        .onAppear {
-            Api().getPosts { (posts) in
-                self.posts = posts
+        List(store.posts) { post in
+            VStack(alignment: .leading, spacing: 8) {
+                Text(post.title).font(.system(.title, design: .serif)).bold()
+                Text(post.body).font(.subheadline).foregroundColor(.secondary)
             }
         }
     }
 }
+//struct PostList: View {
+//    @State var posts: [Post] = []
+//    
+//    var body: some View {
+//        List(posts) { post in
+//            Text(post.title)
+//        }
+//        .onAppear {
+//            Api().getPosts { (posts) in
+//                self.posts = posts
+//            }
+//        }
+//    }
+//}
 
 #Preview {
     PostList()
