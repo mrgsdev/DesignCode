07de94e5e62e2b9c9a2dae7f4cdc362a5847467d
mrgsdev <mrgsdev@gmail.com>
2024-08-10 15:37:33 +0300

    Background Blur

---
+++ SwiftUI/003. Build an app with SwiftUI/Part 2/Build an app with SwiftUI/BlurView.swift
@@ -0,0 +1,34 @@
+//
+//  BlurView.swift
+//  Build an app with SwiftUI
+//
+//  Created by mrgsdev on 10.08.2024.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    typealias UIViewType = UIView
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIView {
+        let view = UIView(frame: CGRect.zero)
+        view.backgroundColor = .clear
+        
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(blurView, at: 0)
+        
+        NSLayoutConstraint.activate([
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<BlurView>) {
+        
+    }
+}
--- SwiftUI/003. Build an app with SwiftUI/Part 2/Build an app with SwiftUI/ContentView.swift
+++ SwiftUI/003. Build an app with SwiftUI/Part 2/Build an app with SwiftUI/ContentView.swift
@@ -216,7 +216,7 @@ struct BottomCardView: View {
         .padding(.top, 8)
         .padding(.horizontal, 20)
         .frame(maxWidth: .infinity)
-        .background(Color.white)
+        .background(BlurView(style: .systemThinMaterial ))
         .cornerRadius(30)
         .shadow(radius: 20)
     }
