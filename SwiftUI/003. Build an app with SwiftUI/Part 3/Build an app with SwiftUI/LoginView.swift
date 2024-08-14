fd658c3d7ffd204c0e2932230b911aa38ee5498a
mrgsdev <mrgsdev@gmail.com>
2024-08-14 13:54:18 +0300

    Login Layout and Variable Font Size

---
+++ SwiftUI/003. Build an app with SwiftUI/Part 3/Build an app with SwiftUI/LoginView.swift
@@ -0,0 +1,60 @@
+//
+//  LoginView.swift
+//  Build an app with SwiftUI
+//
+//  Created by mrgsdev on 14.08.2024.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            Color("background2")
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .edgesIgnoringSafeArea(.bottom)
+            
+            VStack {
+                GeometryReader { geometry in
+                    Text("Learn design & code.\nFrom scratch.")
+                        .font(.system(size: geometry.size.width/10, weight: .bold))
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: 375, maxHeight: 100)
+                .padding(.horizontal, 16)
+                
+                Text("80 hours of courses for SwiftUI, React and design tools.")
+                    .font(.subheadline)
+                    .frame(width: 250)
+                
+                Spacer()
+            }
+            .multilineTextAlignment(.center)
+            .padding(.top, 100)
+            .frame(height: 477)
+            .frame(maxWidth: .infinity)
+            .background(
+                ZStack {
+                    Image(uiImage: #imageLiteral(resourceName: "Blob"))
+                        .offset(x: -150, y: -200)
+                        .blendMode(.plusDarker)
+                    
+                    Image(uiImage: #imageLiteral(resourceName: "Blob"))
+                        .offset(x: -200, y: -250)
+                        .blendMode(.overlay)
+                }
+            )
+            .background(Image(uiImage: #imageLiteral(resourceName: "Card3")), alignment: .bottom)
+            .background(Color(#colorLiteral(red: 0.4117647059, green: 0.4705882353, blue: 0.9725490196, alpha: 1)))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
