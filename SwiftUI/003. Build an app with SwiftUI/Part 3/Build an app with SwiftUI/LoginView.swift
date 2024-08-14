33742831d1d8e9af6f93623cdac32ce37d0a4940
mrgsdev <mrgsdev@gmail.com>
2024-08-14 13:59:49 +0300

    3D Parallax and Looping Animation

--- SwiftUI/003. Build an app with SwiftUI/Part 3/Build an app with SwiftUI/LoginView.swift
+++ SwiftUI/003. Build an app with SwiftUI/Part 3/Build an app with SwiftUI/LoginView.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 
-import SwiftUI
-
 struct LoginView: View {
+    @State var show = false
+    @State var viewState = CGSize.zero
+    @State var isDragging = false
+    
     var body: some View {
         ZStack(alignment: .top) {
             Color.black.edgesIgnoringSafeArea(.all)
@@ -26,10 +28,12 @@ struct LoginView: View {
                 }
                 .frame(maxWidth: 375, maxHeight: 100)
                 .padding(.horizontal, 16)
+                .offset(x: viewState.width/15, y: viewState.height/15)
                 
                 Text("80 hours of courses for SwiftUI, React and design tools.")
                     .font(.subheadline)
                     .frame(width: 250)
+                    .offset(x: viewState.width/20, y: viewState.height/20)
                 
                 Spacer()
             }
@@ -41,16 +45,38 @@ struct LoginView: View {
                 ZStack {
                     Image(uiImage: #imageLiteral(resourceName: "Blob"))
                         .offset(x: -150, y: -200)
+                        .rotationEffect(Angle(degrees: show ? 360+90 : 90))
                         .blendMode(.plusDarker)
+                        .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false))
+                        .onAppear { self.show = true }
                     
                     Image(uiImage: #imageLiteral(resourceName: "Blob"))
                         .offset(x: -200, y: -250)
+                        .rotationEffect(Angle(degrees: show ? 360 : 0), anchor: .leading)
                         .blendMode(.overlay)
+                        .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false))
                 }
             )
-            .background(Image(uiImage: #imageLiteral(resourceName: "Card3")), alignment: .bottom)
+            .background(
+                Image(uiImage: #imageLiteral(resourceName: "Card3"))
+                    .offset(x: viewState.width/25, y: viewState.height/25)
+                , alignment: .bottom
+            )
             .background(Color(#colorLiteral(red: 0.4117647059, green: 0.4705882353, blue: 0.9725490196, alpha: 1)))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .scaleEffect(isDragging ? 0.9 : 1)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+            .rotation3DEffect(Angle(degrees: 5), axis: (x: viewState.width, y: viewState.height, z: 0))
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
+                    self.isDragging = true
+                }
+                .onEnded { value in
+                    self.viewState = .zero
+                    self.isDragging = false
+                }
+            )
         }
     }
 }
