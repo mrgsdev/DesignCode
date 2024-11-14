f222b64591d4203bec562ce4c55f66a6b0809a30
mrgsdev <mrgsdev@gmail.com>
2024-11-14 15:27:15 +0300

    Add Project

---
+++ SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/SwiftUIForiOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIForiOS14App.swift
+//  SwiftUIForiOS14
+//
+//  Created by mrgsdev on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIForiOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
