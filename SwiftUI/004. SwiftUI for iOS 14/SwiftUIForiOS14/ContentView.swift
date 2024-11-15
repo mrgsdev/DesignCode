bb48f90c03b0093556d4ad9d5ee7b5ae46f9a4a5
mrgsdev <mrgsdev@gmail.com>
2024-11-15 16:20:18 +0300

    02. SwiftUI Visual Editor

--- SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/ContentView.swift
+++ SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/ContentView.swift
@@ -9,16 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                Spacer()
+                Image("Illustration 1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+            Text("SwiftUI for iOS 14").fontWeight(.bold).foregroundColor(Color.white)
+            Text("20 Sections").font(.footnote).foregroundColor(Color.white)
         }
-        .padding()
+        .padding(.all)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
     }
 }
 
 #Preview {
-    ContentView()
+    Group {
+        ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 200.0, height: 200.0))
+            .preferredColorScheme(.dark)
+    }
 }
