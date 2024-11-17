4a87eaa6abe3a33045e5d5558e1e14e7295ca8fe
mrgsdev <mrgsdev@gmail.com>
2024-11-17 17:29:04 +0300

    05. macOS app and Toolbar

--- SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/CoursesView.swift
+++ SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/CoursesView.swift
@@ -9,11 +9,21 @@
 import SwiftUI
 
 struct CoursesView: View {
+    @ViewBuilder
     var body: some View {
+        #if os(iOS)
+        content
+            .listStyle(InsetGroupedListStyle())
+        #else
+        content
+            .frame(minWidth: 800, minHeight: 600)
+        #endif
+    }
+    var content: some View {
         List(0 ..< 20) { item in
             CourseRow()
         }
-        .listStyle(InsetGroupedListStyle())
         .navigationTitle("Courses")
     }
 }
+
--- SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/Sidebar.swift
+++ SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/Sidebar.swift
@@ -11,19 +11,27 @@ import SwiftUI
 struct Sidebar: View {
     var body: some View {
         NavigationView {
-            List {
-                NavigationLink(destination: CoursesView()) {
-                    Label("Courses", systemImage: "book.closed")
-                }
-                Label("Tutorials", systemImage: "list.bullet.rectangle")
-                Label("Livestreams", systemImage: "tv")
-                Label("Certificates", systemImage: "mail.stack")
-                Label("Search", systemImage: "magnifyingglass")
-            }
-            .listStyle(SidebarListStyle())
-            .navigationTitle("Learn")
+            #if os(iOS)
+            content
+                .navigationTitle("Learn")
+            #else
+            content
+                .frame(minWidth: 200, idealWidth: 250, maxWidth: 300)
+            #endif
             
             CoursesView()
         }
+    }
+    var content: some View {
+        List {
+            NavigationLink(destination: CoursesView()) {
+                Label("Courses", systemImage: "book.closed")
+            }
+            Label("Tutorials", systemImage: "list.bullet.rectangle")
+            Label("Livestreams", systemImage: "tv")
+            Label("Certificates", systemImage: "mail.stack")
+            Label("Search", systemImage: "magnifyingglass")
+        }
+        .listStyle(SidebarListStyle())
     }
 }
