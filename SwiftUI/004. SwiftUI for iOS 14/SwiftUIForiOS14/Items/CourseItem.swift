d8ea5768ac30f9caa58286fc8e5a3d698c8fb075
mrgsdev <mrgsdev@gmail.com>
2024-11-22 17:23:33 +0300

    10. Matched Cards and Static Data

--- SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/Items/CourseItem.swift
+++ SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/Items/CourseItem.swift
@@ -9,25 +9,28 @@
 import SwiftUI
 
 struct CourseItem: View {
+    var course: Course = courses[0]
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 4.0) {
             Spacer()
             HStack {
                 Spacer()
-                Image("Illustration 1")
+                Image(course.image)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 Spacer()
             }
-            Text("SwiftUI for iOS 14").fontWeight(.bold).foregroundColor(Color.white)
-            Text("20 Sections").font(.footnote).foregroundColor(Color.white)
+            Text(course.title).fontWeight(.bold).foregroundColor(Color.white)
+            Text(course.subtitle).font(.footnote).foregroundColor(Color.white)
         }
         .padding(.all)
-        .background(Color.blue)
-        .cornerRadius(20.0)
-        .shadow(radius: 10)
+        .background(course.color)
+        .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+        .shadow(color: course.color.opacity(0.3), radius: 20, x: 0, y: 10)
     }
 }
+
 
 #Preview {
     CourseItem()
