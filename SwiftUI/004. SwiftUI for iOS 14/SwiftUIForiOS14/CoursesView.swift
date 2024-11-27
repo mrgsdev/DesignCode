be2104d69b8073d8be38f15e162cd2cfb76ff99a
mrgsdev <mrgsdev@gmail.com>
2024-11-27 14:10:20 +0300

    16. Size Class and Tab Bar

--- SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/CoursesView.swift
+++ SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/CoursesView.swift
@@ -32,29 +32,50 @@ struct CoursesView: View {
     
     var content: some View {
         ScrollView {
-            LazyVGrid(
-                columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
-                spacing: 16
-            ) {
-                ForEach(courses) { item in
-                    VStack {
-                        CourseItem(course: item)
-                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                            .frame(height: 200)
-                            .onTapGesture {
-                                withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
-                                    show.toggle()
-                                    selectedItem = item
-                                    isDisabled = true
+            VStack(spacing: 0) {
+                Text("Courses")
+                    .font(.largeTitle)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 16)
+                    .padding(.top, 54)
+                
+                LazyVGrid(
+                    columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
+                    spacing: 16
+                ) {
+                    ForEach(courses) { item in
+                        VStack {
+                            CourseItem(course: item)
+                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                                .frame(height: 200)
+                                .onTapGesture {
+                                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
+                                        show.toggle()
+                                        selectedItem = item
+                                        isDisabled = true
+                                    }
                                 }
-                            }
-                            .disabled(isDisabled)
+                                .disabled(isDisabled)
+                        }
+                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
                     }
-                    .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
                 }
+                .padding(16)
+                .frame(maxWidth: .infinity)
+                
+                Text("Latest sections")
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 240))]) {
+                    ForEach(courseSections) { item in
+                        CourseRow(item: item)
+                    }
+                }
+                .padding()
             }
-            .padding(16)
-            .frame(maxWidth: .infinity)
         }
         .zIndex(1)
     }
@@ -83,7 +104,6 @@ struct CoursesView: View {
         }
     }
 }
-
 #Preview {
     CoursesView()
 }
---
+++ SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/Navigation/TabBar.swift
@@ -0,0 +1,55 @@
+//
+//  TabBar.swift
+//  SwiftUIForiOS14
+//
+//  Created by mrgsdev on 27.11.2024.
+//
+
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            NavigationView {
+                CoursesView()
+            }
+            .tabItem {
+                Image(systemName: "book.closed")
+                Text("Courses")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "list.bullet.rectangle")
+                Text("Tutorials")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "tv")
+                Text("Livestreams")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "mail.stack")
+                Text("Certificates")
+            }
+            
+            NavigationView {
+                CourseList()
+            }
+            .tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("Search")
+            }
+        }
+    }
+}
