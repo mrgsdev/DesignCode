e3544c02f43c5188166ad5a4ff57a93d95c9e3ca
mrgsdev <mrgsdev@gmail.com>
2024-11-30 13:57:22 +0300

    19. Library Content and Modifiers

---
+++ SwiftUI/004. SwiftUI for iOS 14/SwiftUIForiOS14/Library/LibraryContent.swift
@@ -0,0 +1,38 @@
+//
+//  LibraryContent.swift
+//  SwiftUIForiOS14
+//
+//  Created by mrgsdev on 30.11.2024.
+//
+
+
+import SwiftUI
+
+struct LibraryContent: LibraryContentProvider {
+    @LibraryContentBuilder
+    var views: [LibraryItem] {
+        LibraryItem(
+            CloseButton(),
+            title: "Close Button View",
+            category: .control
+        )
+    }
+    
+    @LibraryContentBuilder
+    func modifiers(base: Image) -> [LibraryItem] {
+        LibraryItem(
+            base.cardStyle(color: Color.blue, cornerRadius: 22),
+            title: "Card Style",
+            category: .effect
+        )
+    }
+}
+
+extension View {
+    func cardStyle(color: Color, cornerRadius: CGFloat) -> some View {
+        return self
+            .background(color)
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
+            .shadow(color: color.opacity(0.3), radius: 20, x: 0, y: 10)
+    }
+}
