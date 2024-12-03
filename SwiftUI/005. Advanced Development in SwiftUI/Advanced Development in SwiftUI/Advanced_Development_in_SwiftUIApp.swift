b6f9d267b8d21168ec1ffc5329ba7558b92d9a54
mrgsdev <mrgsdev@gmail.com>
2024-12-03 12:47:11 +0300

    Advanced Development in SwiftUI

---
+++ SwiftUI/005. Advanced Development in SwiftUI/Advanced Development in SwiftUI/Advanced_Development_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_Development_in_SwiftUIApp.swift
+//  Advanced Development in SwiftUI
+//
+//  Created by mrgsdev on 03.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_Development_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/005. Advanced Development in SwiftUI/Advanced Development in SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Advanced Development in SwiftUI
+//
+//  Created by mrgsdev on 03.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
