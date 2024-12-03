00925d53ed2abdc59c91049f99cdb0332e8d01ee
mrgsdev <mrgsdev@gmail.com>
2024-12-03 13:07:17 +0300

    2. Building the Signup Card

--- SwiftUI/005. Advanced Development in SwiftUI/Advanced Development in SwiftUI/ContentView.swift
+++ SwiftUI/005. Advanced Development in SwiftUI/Advanced Development in SwiftUI/ContentView.swift
@@ -6,81 +6,85 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State var email = ""
+    @State var password = ""
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        ZStack {
+            Image("background-3")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+            
+            VStack {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Sign Up")
+                        .font(Font.largeTitle.bold())
+                        .foregroundColor(.white)
+                    Text("Access to 120+ hours of courses, tutorials and livestreams")
+                        .font(.subheadline)
+                        .foregroundColor(.white.opacity(0.7))
+                    HStack(spacing: 12) {
+                        Image(systemName: "envelope.open.fill")
+                            .foregroundStyle(.white)
+                        TextField("Email",text: $email)
+                            .colorScheme(.dark)
+                            .foregroundColor(Color.white.opacity(0.7))
+                            .autocapitalization(.none)
+                            .textContentType(.emailAddress)
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    .frame(height: 52)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16, style: .circular)
+                            .stroke(Color.white, lineWidth: 1)
+                            .blendMode(.overlay)
+                    )
+                    .background(
+                        Color("secondaryBackground")
+                            .cornerRadius(16)
+                            .opacity(0.8)
+                    )
+                    
+                    HStack(spacing: 12) {
+                        Image(systemName: "key.fill")
+                            .foregroundStyle(.white)
+                        TextField("Password",text: $password)
+                            .colorScheme(.dark)
+                            .foregroundColor(Color.white.opacity(0.7))
+                            .autocapitalization(.none)
+                            .textContentType(.password)
                     }
+                    .frame(height: 52)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16, style: .circular)
+                            .stroke(Color.white, lineWidth: 1)
+                            .blendMode(.overlay)
+                    )
+                    .background(
+                        Color("secondaryBackground")
+                            .cornerRadius(16)
+                            .opacity(0.8)
+                    )
                 }
+                .padding(20)
             }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            .background(
+                RoundedRectangle(cornerRadius: 30)
+                    .stroke(.white.opacity(0.2))
+                    .background(Color("secondaryBackground"))
+                    .opacity(0.5)
+                    .background(VisualEffectBlur(blurStyle: .systemMaterialDark))
+                    .shadow(color: Color("shadowColor").opacity(0.5), radius: 60, x: 0, y: 30)
+            )
+            .cornerRadius(30)
+            .padding(.horizontal)
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+ 
 
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    ContentView()
 }
